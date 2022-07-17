e9bdea1e786a8326c65c5ab1ec364b4c383d8ed7
travis flake <tflake83@gmail.com>
2022-07-17 10:01:27 -0400

    added currency formatter

--- src/components/TotalCard.js
+++ src/components/TotalCard.js
@@ -1,19 +1,17 @@
 import React from 'react'
 import './TotalCard.css'
 import MenuTable from './MenuTable'
-
+import { currencyFormatter } from '../assets/currencyFormatter'
 
 export default function TotalCard({total, items}) {
-    
-    
-    
+        
     return (
         <>
         <div>
             <MenuTable items={total, items}/>
         </div>
         <div>
-            <p><strong>Total:</strong> $<span>{total}</span></p>
+            <p><strong>Total:</strong> <span>{currencyFormatter.format(total)}</span></p>
         </div>
         </>
     )
